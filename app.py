1ad0458fcc8dfec28d579bf7188818d3aa106374
rrsc160 <rrsc160@gmail.com>
2025-04-02 12:12:02 +0530

    Update app.py
    gdg

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ def optimize():
         'sustainabilityScore': round(sustainability_score, 2)
     })
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=10000)
+    app.run(debug=True)
+# if __name__ == '__main__':
+#     from waitress import serve
+#     serve(app, host='0.0.0.0', port=10000)
